7db4cce3f26d3d3e894d789babd900e6218d4d36
SebastianHermann <sebi.hermann95@t-online.de>
2021-02-19 00:19:25 +0100

    Start Script changed v4

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../Frontend/build'));
+  app.use(express.static('/client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../Frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/client', 'build', 'index.html'));
   });
 }
 
