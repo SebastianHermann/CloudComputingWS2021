0be766e46090dcc56cb96d9f4c43e77fa30367db
SebastianHermann <sebi.hermann95@t-online.de>
2021-02-18 23:58:47 +0100

    Start Script changed v2

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ mongoose
   .catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/Frontend/build'));
+  app.use(express.static('../Frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/Frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../Frontend', 'build', 'index.html'));
   });
 }
 
